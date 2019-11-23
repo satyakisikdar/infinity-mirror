9febd350c5ddc04f8063f743dc86635442ff673e
Satyaki Sikdar <ssikdar@nd.edu>
2019-11-23 15:22:26 -0500

    makes the directories input and output if needed

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import os
+
 from src.graph_io import GraphReader, GraphWriter
 
+def make_dirs():
+    '''
+    Makes input and output directories if they do not exist already
+    :return:
+    '''
+    for dirname in ('input', 'output'):
+        if not os.path.exists(f'./{dirname}'):
+            os.makedirs(f'./{dirname}')
+
 def main():
+    make_dirs()
     graph_reader = GraphReader(filename='./input/karate.mat')
     graph = graph_reader.graph
 
