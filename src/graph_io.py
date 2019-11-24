a502aa1e5148a2a56e3508db3286b774a69d24d9
Satyaki Sikdar <ssikdar@nd.edu>
2019-11-24 15:07:29 -0500

    added pyintergraph interfaces to convert to and from igraph, graph-tool, and NetworkX graphs

--- src/graph_io.py
+++ src/graph_io.py
@@ -1,25 +1,27 @@
 '''
 Grpah i/o helpers
 '''
-
 import networkx as nx
 from pathlib import Path
 import numpy as np
+import pyintergraph as pig
+import igraph as ig
 
 from src.print_utils import ColorPrint as CP
 
+
 class GraphReader:
-    '''
+    """
     Class for graph reader
     .g /.txt: graph edgelist
     .gml, .gexf for Gephi
     .mat for adjacency matrix
-    '''
+    """
     def __init__(self, filename: str, gname: str=''):
-        '''
+        """
         :param filename: path to input file
         :param gname: name of the graph
-        '''
+        """
         self.possible_extensions = ['.g', '.gexf', '.gml', '.txt', '.mat']
         self.filename = filename
         self.path = Path(filename)
@@ -33,10 +35,10 @@ class GraphReader:
         assert self.graph.name != '', 'Graph name is empty'
 
     def _read(self) -> nx.Graph:
-        '''
+        """
         Reads the graph based on its extension
         :return:
-        '''
+        """
         extension = self.path.suffix
 
         assert extension in self.possible_extensions, f'Invalid extension {extension}, supported extensions: ' \
@@ -62,11 +64,17 @@ class GraphReader:
         CP.print_green(f'Reading {self.gname} from {self.path} with n={graph.order():,}, m={graph.size():,}')
         return graph
 
+    def __str__(self):
+        return f'<GraphReader object> graph: {self.gname}, path: {str(self.path)} n={self.graph.order():,}, m={self.graph.size()}'
+
+    def __repr__(self):
+        return str(self)
+
 
 class GraphWriter:
-    '''
+    """
     Class for writing graphs, expects a networkx graph as input
-    '''
+    """
     def __init__(self, graph: nx.Graph, path: str, fmt: str='', gname: str=''):
         self.graph = graph
 
@@ -83,10 +91,10 @@ class GraphWriter:
         self._write()
 
     def _write(self):
-        '''
+        """
         write the graph into the format
         :return:
-        '''
+        """
         extension = self.path.suffix
         str_path = str(self.path)
 
@@ -104,4 +112,29 @@ class GraphWriter:
             np.savetxt(fname=self.path, X=mat, fmt='%d')
 
         CP.print_blue(f'Wrote {self.graph.name} to {self.path} with n={self.graph.order():,}, m={self.graph.size():,}')
+
+    def __str__(self):
+        return f'<GraphWriter object> graph: {self.graph}, path: {str(self.path)} n={self.graph.order():,}, m={self.graph.size():,}'
+
+    def __repr__(self):
+        return str(self)
+
+
+def networkx_to_graphtool(nx_G: nx.Graph):
+    return pig.nx2gt(nx_G, labelname='node_label')
+
+
+def graphtool_to_networkx(gt_G):
+    graph = pig.InterGraph.from_graph_tool(gt_G)
+    return graph.to_networkX()
+
+
+def networkx_to_igraph(nx_G: nx.Graph):
+    graph = pig.InterGraph.from_networkX(nx_G)
+    return graph.to_igraph()
+
+
+def igraph_to_networkx(ig_G: ig.Graph):
+    graph = pig.InterGraph.from_igraph(ig_G)
+    return graph.to_networkX()
 
