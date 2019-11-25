f0cfceb6df17d9e6259ac950d73b32f7499b8145
Satyaki Sikdar <ssikdar@nd.edu>
2019-11-25 14:59:06 -0500

    added BTER test code

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import os
 
 from src.graph_io import GraphReader, GraphWriter
-from src.graph_models import ErdosRenyi, ChungLu
+from src.graph_models import ErdosRenyi, ChungLu, BTER
 
 def make_dirs():
     '''
     Makes input and output directories if they do not exist already
     :return:
     '''
-    for dirname in ('input', 'output'):
+    for dirname in ('input', 'output', 'analysis'):
         if not os.path.exists(f'./{dirname}'):
             os.makedirs(f'./{dirname}')
 
 
 def test_generators(g):
     er = ErdosRenyi(input_graph=g)
-    er.fit()
-    er.generate_graphs(10)
+    er.generate(10)
     print(er)
 
     cl = ChungLu(input_graph=g)
-    cl.fit()
-    cl.generate_graphs(10)
+    cl.generate(10)
     print(cl)
+
+    bter = BTER(input_graph=g)
+    bter.generate(1)
+    print(bter)
 
 
 def main():
