cd312cb39bbf173d0022955c8f70d6cb92c6e8dd
Satyaki Sikdar <ssikdar@nd.edu>
2019-11-26 20:38:22 -0500

    major changes. added lcc and reindexing stuff

--- src/graph_io.py
+++ src/graph_io.py
@@ -7,8 +7,7 @@ import numpy as np
 import pyintergraph as pig
 import igraph as ig
 
-from src.utils import ColorPrint as CP
-from src.utils import check_file_exists
+from src.utils import ColorPrint as CP, check_file_exists, print_float
 
 
 class GraphReader:
@@ -18,7 +17,7 @@ class GraphReader:
     .gml, .gexf for Gephi
     .mat for adjacency matrix
     """
-    def __init__(self, filename: str, gname: str=''):
+    def __init__(self, filename: str, gname: str='', reindex_nodes: bool=False, first_label: int=0, take_lcc: bool=True):
         """
         :param filename: path to input file
         :param gname: name of the graph
@@ -26,26 +25,29 @@ class GraphReader:
         self.possible_extensions = ['.g', '.gexf', '.gml', '.txt', '.mat']
         self.filename = filename
         self.path = Path(filename)
-        assert check_file_exists(self.path), f'Path: {self.path} does not exist'
+        assert check_file_exists(self.path), f'Path: "{self.path}" does not exist'
 
         if gname != '':
             self.gname = gname
         else:
             self.gname = self.path.stem
-        self.graph = self._read()
+
+        self.graph: nx.Graph = self._read()
+        self._preprocess(reindex_nodes=reindex_nodes, first_label=first_label, take_lcc=take_lcc)
         assert self.graph.name != '', 'Graph name is empty'
+
 
     def _read(self) -> nx.Graph:
         """
         Reads the graph based on its extension
+        returns the largest connected component
         :return:
         """
+        CP.print_blue(f'Reading "{self.gname}" from "{self.path}"')
         extension = self.path.suffix
-
-        assert extension in self.possible_extensions, f'Invalid extension {extension}, supported extensions: ' \
+        assert extension in self.possible_extensions, f'Invalid extension "{extension}", supported extensions: ' \
                                                       f'{self.possible_extensions}'
 
-        graph = nx.Graph()
         str_path = str(self.path)
 
         if extension in ('.g', '.txt'):
@@ -60,15 +62,48 @@ class GraphReader:
         elif extension == '.mat':
             mat = np.loadtxt(fname=str_path, dtype=bool)
             graph: nx.Graph = nx.from_numpy_array(mat)
+        else:
+            raise(NotImplementedError, f'{extension} not supported')
 
         graph.name = self.gname
-        CP.print_green(f'Reading {self.gname} from {self.path} with n={graph.order():,}, m={graph.size():,}')
         return graph
 
-    def __str__(self):
+    def _preprocess(self, reindex_nodes: bool, first_label: int=0, take_lcc: bool=True) -> None:
+        """
+        Preprocess the graph - taking the largest connected components, re-index nodes if needed
+        :return:
+        """
+        CP.print_green('Pre-processing graph....')
+        CP.print_blue(f'Original graph "{self.gname}" n:{self.graph.order():,} '
+                        f'm:{self.graph.size():,} #components: {nx.number_connected_components(self.graph)}')
+
+        if take_lcc and nx.number_connected_components(self.graph) > 1:
+            ## Take the LCC
+            component_sizes = [len(c) for c in sorted(nx.connected_components(self.graph), key=len, reverse=True)]
+
+            CP.print_green(f'Taking the largest component out of {len(component_sizes)} components: {component_sizes}')
+
+            graph_lcc = self.graph.subgraph(max(nx.connected_components(self.graph), key=len))
+
+            perc_nodes = graph_lcc.order() / self.graph.order() * 100
+            perc_edges = graph_lcc.size() / self.graph.size() * 100
+            CP.print_orange(f'LCC has {print_float(perc_nodes)}% of nodes and {print_float(perc_edges)}% edges in the original graph')
+
+            self.graph = graph_lcc
+
+        if reindex_nodes:
+            # re-index nodes, stores the old label in old_label
+            self.graph = nx.convert_node_labels_to_integers(self.graph, first_label=first_label,
+                                                            label_attribute='old_label')
+            CP.print_green(f'Re-indexing nodes to start from {first_label}, old labels are stored in node attr "old_label"')
+
+        CP.print_blue(f'Pre-processed graph "{self.gname}" n:{self.graph.order():,} m:{self.graph.size():,}')
+        return
+
+    def __str__(self) -> str:
         return f'<GraphReader object> graph: {self.gname}, path: {str(self.path)} n={self.graph.order():,}, m={self.graph.size()}'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
 
 
@@ -91,7 +126,7 @@ class GraphWriter:
             self.fmt = fmt
         self._write()
 
-    def _write(self):
+    def _write(self) -> None:
         """
         write the graph into the format
         :return:
@@ -113,11 +148,12 @@ class GraphWriter:
             np.savetxt(fname=self.path, X=mat, fmt='%d')
 
         CP.print_blue(f'Wrote {self.graph.name} to {self.path} with n={self.graph.order():,}, m={self.graph.size():,}')
+        return
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'<GraphWriter object> graph: {self.graph}, path: {str(self.path)} n={self.graph.order():,}, m={self.graph.size():,}'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
 
 
