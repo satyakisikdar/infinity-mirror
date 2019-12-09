afc3fa8d30f14810d407ecb0ec13ed1d119a203e
Satyaki Sikdar <ssikdar@nd.edu>
2019-12-08 22:00:31 -0500

    infinity mirror base class

---
+++ src/infinity_mirror.py
@@ -0,0 +1,21 @@
+import networkx as nx
+
+from src.graph_models import *
+
+class InfinityMirror:
+    def __init__(self, input_graph: nx.Graph, model: str, generations: int)
+        self.input_graph = input_graph
+        self.model_name = model  # graph_model is a GraphModel object
+        self.generations = generations
+        self.init()
+
+    def init(self):
+
+
+    def _get_one_generation(self, input_graph, model):
+        # step 1: get input graph
+        # step 2: fit model
+        # step 3: generate output graphs
+
+        return
+
