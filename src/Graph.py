ebc689f595b129a200bf4defe9f29d230c294a70
Satyaki Sikdar <ssikdar@nd.edu>
2019-12-19 23:56:50 -0500

    added plot options for Graph and InfinityMirror objects

--- src/Graph.py
+++ src/Graph.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class CustomGraph(nx.Graph):
@@ -9,9 +10,25 @@ class CustomGraph(nx.Graph):
         super().__init__(incoming_graph_data=incoming_graph_data, **attr)
         if 'gen_id' in attr:
             self.gen_id = attr['gen_id']
+        else:
+            self.gen_id = 0
 
     def __str__(self):
-        return f'"{self.name}" gen: {self.gen_id} n:{self.order():_} m:{self.size():_}'
+        return f'"{self.name}" gen:{self.gen_id} n:{self.order():_} m:{self.size():_}'
 
     def __repr__(self):
         return str(self)
+
+    def plot(self, ax=None, prog: str='neato', update_pos: bool=True, pos={}, title=None):
+        if update_pos or len(pos) == 0:
+            pos = nx.nx_agraph.graphviz_layout(self, prog=prog)
+
+        nx.draw_networkx_nodes(self, pos=pos, alpha=0.5, ax=ax, node_size=20)
+        # nx.draw_networkx_labels(self, pos=pos, font_color='w', ax=ax, labels=False)
+        nx.draw_networkx_edges(self, pos=pos, ax=ax, alpha=0.15)
+
+        if title is None:
+            title = str(self)
+        plt.title(title)
+        # plt.style.use('seaborn-white')
+        plt.grid(False)
--- src/infinity_mirror.py
+++ src/infinity_mirror.py
@@ -1,17 +1,22 @@
 import networkx as nx
 
+from collections import defaultdict
 from typing import Any, List, Dict
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import math
+
 from src.graph_models import *
-from src.graph_models import BaseGraphModel
 from src.Graph import CustomGraph
+from src.graph_io import GraphReader
+
 
 class InfinityMirror:
     def __init__(self, initial_graph: CustomGraph, model_obj: Any, num_generations: int):
-        self.initial_graph = initial_graph  # the initial starting point H_0
-        self.num_generations = num_generations  # number of generations
-        self.model_obj = model_obj  # BaseGraphModel object
-        self.model: BaseGraphModel = self.init_model()  # init BaseGraphModel object based on the parameters
-        self.current_generation = 0  # counter for current generations
+        self.initial_graph: CustomGraph = initial_graph  # the initial starting point H_0
+        self.num_generations: int = num_generations  # number of generations
+        self.model: BaseGraphModel = self.init_model(model_obj)  # init BaseGraphModel object based on the parameters
+        self.current_generation: int = 0  # counter for current generations
         self.graphs_by_generation: Dict[int, List[CustomGraph]] = {0: [self.initial_graph]}
 
     def __str__(self):
@@ -20,11 +25,11 @@ class InfinityMirror:
     def __repr__(self):
         return str(self)
 
-    def init_model(self) -> BaseGraphModel:
+    def init_model(self, model_obj) -> BaseGraphModel:
         """
         Initializes Infinity Mirror object - calls the right Model object
         """
-        return self.model_obj(input_graph=self.initial_graph)  # initialize and fit the model
+        return model_obj(input_graph=self.initial_graph)  # initialize and fit the model
 
     def _get_next_generation(self, num_graphs: int=1) -> None:
         """
@@ -50,13 +55,47 @@ class InfinityMirror:
             self._get_next_generation(num_graphs=num_graphs)
         return
 
-def main():
-    g = nx.path_graph(10)
-    g = CustomGraph(g, gen_id=0)
-    g.name = f'path_graph'
+    def plot(self, prog: str = 'neato'):
+        """
+        Plot the progression of the infinity mirror - fix the node positions
+        :return:
+        """
+        pos = defaultdict(lambda: (0, 0))  # the default dict
+        pos.update(nx.nx_agraph.graphviz_layout(self.initial_graph, prog=prog))  # get the pos of nodes of the original nodes
 
-    inf = InfinityMirror(initial_graph=g, num_generations=5, model_obj=ErdosRenyi)
-    inf.run()
+
+        N = self.num_generations
+        cols = 2
+        rows = int(math.ceil(N / cols))
+
+        gs = gridspec.GridSpec(rows, cols)
+        fig = plt.figure()
+        for i in range(N):
+            graph: CustomGraph = self.graphs_by_generation[i][0]  # pick the 1st graph by default
+            ax = fig.add_subplot(gs[i])
+            graph.plot(ax=ax, pos=pos, update_pos=False)
+
+        fig.tight_layout()
+        fig.suptitle(self.model.model_name)
+        plt.grid(False)
+        # plt.title(self.model.model_name)
+        plt.show()
+
+
+def main():
+    # g = nx.path_graph(20)
+    # g = CustomGraph(g, gen_id=0)
+    # g.name = f'ladder_graph'
+
+    graph_reader = GraphReader(filename='../input/karate.g', reindex_nodes=True, first_label=0)
+    g = graph_reader.graph
+
+    inf = InfinityMirror(initial_graph=g, num_generations=5, model_obj=CNRG)
+
+    # inf.graphs_by_generation[0][0].plot(prog='neato')
+    # plt.style.use('seaborn-white')
+    # plt.grid(False)
+    # plt.show()
     print(inf)
 
 
