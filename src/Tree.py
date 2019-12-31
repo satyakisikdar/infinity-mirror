45b153ba0b98863e5e9371ad2a399ac534871a2d
Satyaki Sikdar <ssikdar@nd.edu>
2019-12-31 16:44:19 -0600

    TreeNode has a new stats variable to store the comparison stats

--- src/Tree.py
+++ src/Tree.py
@@ -1,12 +1,18 @@
-from anytree import Node, RenderTree
+from typing import Dict
+
+from anytree import Node
+
+from src.Graph import CustomGraph
+
 
 class TreeNode(Node):
     """
     Extends the Node class from anytree to store the graph
     """
-    def __init__(self, name, graph, parent=None, children=None, **kwargs) -> None:
+    def __init__(self, name: str, stats: Dict[str, float], graph: CustomGraph, parent=None, children=None, **kwargs) -> None:
         super(TreeNode, self).__init__(name, parent, children, **kwargs)
-        self.graph = graph
+        self.graph: CustomGraph = graph
+        self.stats: Dict[str, float] = stats
         return
 
     def __eq__(self, other) -> bool:
