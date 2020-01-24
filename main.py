8052bbcc76cea581284f3407b9ec52d8218ebb3f
Satyaki Sikdar <ssikdar@nd.edu>
2020-01-23 19:03:20 -0500

    added global numpy thread restrictions - 2 threads at max

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import ast
 import glob
 import importlib
 import multiprocessing
-import os
 import time
 from pathlib import Path
 from typing import Any
 
 import networkx as nx
 from joblib import Parallel, delayed
+
+import os
+os.environ["OMP_NUM_THREADS"] = "2" # export OMP_NUM_THREADS=4
+os.environ["OPENBLAS_NUM_THREADS"] = "2" # export OPENBLAS_NUM_THREADS=4
+os.environ["MKL_NUM_THREADS"] = "2" # export MKL_NUM_THREADS=6
+os.environ["VECLIB_MAXIMUM_THREADS"] = "2" # export VECLIB_MAXIMUM_THREADS=4
+os.environ["NUMEXPR_NUM_THREADS"] = "2" # export NUMEXPR_NUM_THREADS=6
 
 from src.graph_io import GraphReader, SyntheticGraph
 from src.infinity_mirror import InfinityMirror
