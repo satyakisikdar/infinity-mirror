716d3bacf0907a85153184d1c93b7487d116109a
Satyaki Sikdar <ssikdar@nd.edu>
2020-02-04 14:13:30 -0500

    changed max workers = 0

--- src/graphrnn/args.py
+++ src/graphrnn/args.py
@@ -61,7 +61,7 @@ class Args():
         self.num_layers = 4
 
         ### training config
-        self.num_workers = 16 # num workers to load data, default 4
+        self.num_workers = 0 # SS: dont use multithreading ; num workers to load data, default 4
         self.batch_ratio = 32 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
         self.epochs = 30 # now one epoch means self.batch_ratio x batch_size, default 3000
         self.epochs_test_start = 5 # default 100
