4dfd533a39293803c61605210de41dbf6f6f4347
Satyaki Sikdar <ssikdar@nd.edu>
2020-02-06 22:46:10 -0500

    reset graphrnn default parameters

--- src/graphrnn/args.py
+++ src/graphrnn/args.py
@@ -63,14 +63,16 @@ class Args():
         ### training config
         self.num_workers = 0 # SS: dont use multithreading ; num workers to load data, default 4
         self.batch_ratio = 32 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        self.epochs = 5 # now one epoch means self.batch_ratio x batch_size, default 3000
-        self.epochs_test_start = 100 # default 100
-        self.epochs_test = 5 # default 100
-        self.epochs_log = 1 # how often to output training information, default 100
-        self.epochs_save = 5 # how often to save output graphs, default 100
+        self.num_workers = 0  # num workers to load data, default 4
+        self.batch_ratio = 32  # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
+        self.epochs = 3000  # now one epoch means self.batch_ratio x batch_size
+        self.epochs_test_start = 100
+        self.epochs_test = 100
+        self.epochs_log = 100_000
+        self.epochs_save = 100_000
 
         self.lr = 0.003
-        self.milestones = [15, 25] # default [400, 1000]
+        self.milestones = [400, 1000]
         self.lr_rate = 0.3
 
         self.sample_time = 2 # sample time in each time step, when validating
