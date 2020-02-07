de0a870c135be92ba98559771e3a9f9aa8e0ed52
Satyaki Sikdar <ssikdar@nd.edu>
2020-02-07 13:17:35 -0500

    added AttributeError in try block

--- src/graph_models.py
+++ src/graph_models.py
@@ -714,7 +714,7 @@ class NetGAN(BaseGraphModel):
         sparse_adj = nx.to_scipy_sparse_matrix(self.input_graph)
         try:
             scores, tg_sum = fit(sparse_adj)
-        except (IndexError, AssertionError):
+        except (IndexError, AssertionError, AttributeError):
             CP.print_orange('NetGAN fit failed')
             scores, tg_sum = None, None
 
