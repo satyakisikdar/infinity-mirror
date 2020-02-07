1a0799ac1d43828294227824c406bbd02443c63b
Satyaki Sikdar <ssikdar@nd.edu>
2020-02-07 15:43:09 -0500

    added Tensorflow close session

--- src/netgan/netgan/netgan.py
+++ src/netgan/netgan/netgan.py
@@ -560,6 +560,8 @@ class NetGAN:
         #### Training completed.
         log_dict = {"disc_losses": disc_losses, 'gen_losses': gen_losses, 'val_performances': val_performances,
                     'edge_overlaps': eo, 'generated_graphs': graphs}
+
+        self.session.close()  # close the interactive session
         return log_dict
 
 
