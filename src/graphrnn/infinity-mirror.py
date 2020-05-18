cb39246ea8925c7fd1e44737daf54d6aed41ae21
cedré <daniel.gonzalez.cedre@gmail.com>
2020-05-18 10:22:15 -0400

    commented errant CP 	modified:   infinity-mirror.py

--- src/graphrnn/infinity-mirror.py
+++ src/graphrnn/infinity-mirror.py
@@ -1,16 +1,17 @@
 """
 Fit method for GraphRNN
 this is slightly different from the normal ones
-1. Fit should take a list of 1,024 graphs and return a model object
-2. Gen should take a model object and generate 1,024 graphs
+1. Fit should take a list of 50 graphs and return a model object
+2. Gen should take a model object and generate 50 graphs
 """
 
 from typing import List
 from train import *
+from pathlib import Path
 
 def fit(graphs: List[nx.Graph], model_type: str, gname: str, gen_id: int):
     note_dict = {'mlp': 'GraphRNN_MLP', 'rnn': 'GraphRNN_RNN'}
-    assert len(graphs) == 1_024, f'Expected graphs: 1024, got: {len(graphs)}'
+    assert len(graphs) == 50, f'Expected graphs: 50, got: {len(graphs)}'
     assert model_type in note_dict
 
     args = Args(graph_type=gname, note=note_dict[model_type.lower()])
@@ -90,12 +91,107 @@ def fit(graphs: List[nx.Graph], model_type: str, gname: str, gen_id: int):
     print()
     return output_graphs
 
+def init(filename: str, gname: str = '', reindex_nodes: bool = False, first_label: int = 0, take_lcc: bool = True) -> nx.Graph:
+    """
+    :param filename: path to input file
+    :param gname: name of the graph
+    """
+    possible_extensions = ['.g', '.gexf', '.gml', '.txt', '.mat']
+    filename = filename
+    path = Path(filename)
+    #assert check_file_exists(path), f'Path: "{self.path}" does not exist'
+
+    if gname == '':
+        gname = path.stem
+
+    graph: nx.Graph = read(path, gname)
+    graph: nx.Graph = preprocess(graph, reindex_nodes=reindex_nodes, first_label=first_label, take_lcc=take_lcc)
+    assert graph.name != '', 'Graph name is empty'
+    return graph
+
+def read(path: str, gname: str) -> nx.Graph:
+    """
+    Reads the graph based on its extension
+    returns the largest connected component
+    :return:
+    """
+    #CP.print_blue(f'Reading "{self.gname}" from "{self.path}"')
+    extension = path.suffix
+    #assert extension in possible_extensions, f'Invalid extension "{extension}", supported extensions: {possible_extensions}'
+
+    str_path = str(path)
+
+    if extension in ('.g', '.txt'):
+        graph: nx.Graph = nx.read_edgelist(str_path, nodetype=int)
+
+    elif extension == '.gml':
+        graph: nx.Graph = nx.read_gml(str_path)
+
+    elif extension == '.gexf':
+        graph: nx.Graph = nx.read_gexf(str_path)
+
+    elif extension == '.mat':
+        mat = np.loadtxt(fname=str_path, dtype=bool)
+        graph: nx.Graph = nx.from_numpy_array(mat)
+    else:
+        raise (NotImplementedError, f'{extension} not supported')
+
+    graph.name = gname
+    return graph
+
+def preprocess(graph: nx.Graph, reindex_nodes: bool, first_label: int = 0, take_lcc: bool = True) -> nx.Graph:
+    """
+    Preprocess the graph - taking the largest connected components, re-index nodes if needed
+    :return:
+    """
+    #CP.print_none('Pre-processing graph....')
+    #CP.print_none(f'Original graph "{self.gname}" n:{self.graph.order():,} '
+    #              f'm:{self.graph.size():,} #components: {nx.number_connected_components(self.graph)}')
+
+    if take_lcc and nx.number_connected_components(graph) > 1:
+        ## Take the LCC
+        component_sizes = [len(c) for c in sorted(nx.connected_components(graph), key=len, reverse=True)]
+
+        #CP.print_none(f'Taking the largest component out of {len(component_sizes)} components: {component_sizes}')
+
+        graph_lcc = nx.Graph(graph.subgraph(max(nx.connected_components(graph), key=len)))
+
+        perc_nodes = graph_lcc.order() / graph.order() * 100
+        perc_edges = graph_lcc.size() / graph.size() * 100
+        #CP.print_orange(f'LCC has {print_float(perc_nodes)}% of nodes and {print_float(perc_edges)}% edges in the original graph')
+
+        graph = graph_lcc
+
+    selfloop_edges = list(nx.selfloop_edges(graph))
+    if len(selfloop_edges) > 0:
+        #CP.print_none(f'Removing {len(selfloop_edges)} self-loops')
+        graph.remove_edges_from(selfloop_edges)  # remove self-loops
+
+    if reindex_nodes:
+        # re-index nodes, stores the old label in old_label
+        graph = nx.convert_node_labels_to_integers(graph, first_label=first_label,
+                                                        label_attribute='old_label')
+        #CP.print_none(
+        #    f'Re-indexing nodes to start from {first_label}, old labels are stored in node attr "old_label"')
+
+    #CP.print_none(f'Pre-processed graph "{self.gname}" n:{self.graph.order():,} m:{self.graph.size():,}')
+    return graph
 
 if __name__ == '__main__':
-    # g = nx.karate_club_graph(); gname = 'karate'
-    g = nx.ring_of_cliques(500, 4); gname = 'ring-cliq-500-4'
-    g.name = gname
-    graphs = [nx.Graph(g) for _ in range(1024)]
+    size = 10
+    ratio = 5
+    if False:
+        #g = nx.karate_club_graph(); gname = 'karate'
+        n = 100
+        g = nx.ring_of_cliques(n, 4); gname = f'ring-cliq-{n}-4_size{size}_ratio{ratio}'
+        g.name = gname
+        graphs = [nx.Graph(g) for _ in range(50)]
+    else:
+        base_path = '/home/danielgonzalez/repos/infinity-mirror/input/'
+        dataset = 'eucore'
+        g = init(os.path.join(base_path, dataset + '.g'))
+        graphs = [nx.Graph(g) for _ in range(50)]; gname = f'{dataset}_size{size}_ratio{ratio}'
+
     model_type = 'mlp'
 
     for gen_id in range(1, 21):
