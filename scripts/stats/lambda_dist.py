0c3f5085b541459b62be18642d38515c54f214c2
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-05-31 16:42:58 -0400

    small changes 	modified:   jensen_shannon.py 	new file:   lambda_dist.py

--- scripts/stats/lambda_dist.py
+++ scripts/stats/lambda_dist.py
@@ -41,28 +41,28 @@ def compute_graph_stats(root):
 def absolute(graph_stats):
     for gs in graph_stats[1:]:
         comparator = GraphPairCompare(graph_stats[0], gs)
-        dist = comparator.js_distance()
+        dist = comparator.lambda_dist()
         yield dist
 
 def sequential(graph_stats):
     prev = graph_stats[0]
     for curr in graph_stats[1:]:
         comparator = GraphPairCompare(prev, curr)
-        dist = comparator.js_distance()
+        dist = comparator.lambda_dist()
         yield dist
         prev = curr
 
-def absolute_js(graph_stats):
+def absolute_ld(graph_stats):
     print('absolute... ', end='', flush=True)
-    abs_js = [x for x in absolute(graph_stats)]
+    abs_ld = [x for x in absolute(graph_stats)]
     print('done')
-    return abs_js
+    return abs_ld
 
-def sequential_js(graph_stats):
+def sequential_ld(graph_stats):
     print('sequential... ', end='', flush=True)
-    seq_js = [x for x in sequential(graph_stats)]
+    seq_ld = [x for x in sequential(graph_stats)]
     print('done')
-    return seq_js
+    return seq_ld
 
 def length_chain(root):
     return len(root.descendants)
@@ -70,20 +70,20 @@ def length_chain(root):
 def flatten(L):
     return [item for sublist in L for item in sublist]
 
-def compute_stats(js):
-    padding = max(len(l) for l in js)
-    for idx, l in enumerate(js):
-        while len(js[idx]) < padding:
-            js[idx] += [np.NaN]
-    mean = np.nanmean(js, axis=0)
+def compute_stats(ld):
+    padding = max(len(l) for l in ld)
+    for idx, l in enumerate(ld):
+        while len(ld[idx]) < padding:
+            ld[idx] += [np.NaN]
+    mean = np.nanmean(ld, axis=0)
     ci = []
-    for row in np.asarray(js).T:
+    for row in np.asarray(ld).T:
         ci.append(st.t.interval(0.95, len(row)-1, loc=np.mean(row), scale=st.sem(row)))
     return np.asarray(mean), np.asarray(ci)
 
-def construct_table(abs_js, seq_js, model):
-    abs_mean, abs_ci = compute_stats(abs_js)
-    seq_mean, seq_ci = compute_stats(seq_js)
+def construct_table(abs_ld, seq_ld, model):
+    abs_mean, abs_ci = compute_stats(abs_ld)
+    seq_mean, seq_ci = compute_stats(seq_ld)
     gen = [x + 1 for x in range(len(abs_mean))]
 
     rows = {'model': model, 'gen': gen, 'abs_mean': abs_mean, 'abs-95%': abs_ci[:,0], 'abs+95%': abs_ci[:,1], 'seq_mean': seq_mean, 'seq-95%': seq_ci[:,0], 'seq+95%': seq_ci[:,1]}
@@ -94,21 +94,21 @@ def construct_table(abs_js, seq_js, model):
 def main():
     base_path = '/data/infinity-mirror'
     dataset = 'eucore'
-    models = ['GCN_AE']
+    models = ['BTER']
     model = models[0]
 
-    output_path = os.path.join(base_path, dataset, models[0], 'jensen-shannon')
-    mkdir(output_path)
+    output_path = os.path.join(base_path, dataset, models[0], 'lambda-dist')
+    mkdir_output(output_path)
 
-    abs_js = []
-    seq_js = []
+    abs_ld = []
+    seq_ld = []
     gen = []
     for root, model in load_data(base_path, dataset, models, True, False):
         graph_stats = compute_graph_stats(root)
-        abs_js.append(absolute_js(graph_stats))
-        seq_js.append(sequential_js(graph_stats))
+        abs_ld.append(absolute_ld(graph_stats))
+        seq_ld.append(sequential_ld(graph_stats))
 
-    df = construct_table(abs_js, seq_js, model)
+    df = construct_table(abs_ld, seq_ld, model)
     df.to_csv(f'{output_path}/{dataset}_{model}', float_format='%.7f', sep='\t', index=False, na_rep='nan')
 
     return
