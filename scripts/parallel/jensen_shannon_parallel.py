dcf9f41368782210d6a59c7a02a5407685762a22
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-05-30 23:45:57 -0400

    🔥 added support for multiple models 🔥 	modified:   jensen_shannon_parallel.py

--- scripts/parallel/jensen_shannon_parallel.py
+++ scripts/parallel/jensen_shannon_parallel.py
@@ -12,8 +12,8 @@ import scipy.stats as st
 
 from tqdm import tqdm
 
-sys.path.append('./../dgonza26/infinity-mirror')
-sys.path.append('..')
+#sys.path.append('./../dgonza26/infinity-mirror')
+sys.path.append('./../..')
 
 from src.graph_comparison import GraphPairCompare
 from src.graph_stats import GraphStats
@@ -96,12 +96,13 @@ def construct_table(abs_js, seq_js, gen, model):
 
 def get_filenames(base_path, dataset, models):
     filenames = []
+    print('loading {dataset} {models[0]}')
     for model in models:
         path = os.path.join(base_path, dataset, model)
         for subdir, dirs, files in os.walk(path):
             for filename in files:
                 if 'seq' in filename and 'rob' not in filename:
-                    print(f'loading {filename}')
+                    #print(f'loading {filename}')
                     filenames.append(os.path.join(subdir, filename))
                     # yield load_pickle(os.path.join(subdir, filename))
     ColorPrint.print_bold(f"Found {len(filenames)} graph files to be loaded.")
@@ -129,93 +130,94 @@ def driver():
 if __name__ == '__main__':
     base_path = '/data/infinity-mirror'
     dataset = 'eucore'
-    models = ['BTER']
+    models_list = ['BTER', 'BUGGE', 'Chung-Lu', 'CNRG']
     num = 10
 
-    output_path = os.path.join(base_path, dataset, models[0], 'jensen-shannon')
-    mkdir_output(output_path)
+    for model in models_list:
+        models = [model]
+        output_path = os.path.join(base_path, dataset, models[0], 'jensen-shannon')
+        mkdir_output(output_path)
 
-    filenames = get_filenames(base_path, dataset, models)
-    graphs_list = []
+        filenames = get_filenames(base_path, dataset, models)
+        graphs_list = []
 
-    results_lock = threading.Lock()
+        results_lock = threading.Lock()
 
-    # pandas dict variables
-    abs_js = []
-    seq_js = []
-    gen = []
+        # pandas dict variables
+        abs_js = []
+        seq_js = []
+        gen = []
 
-    read_pbar = tqdm(len(filenames), desc="Reading Files", position=0, leave=False)
-    work_pbar = tqdm(len(filenames), desc="Processing Files", position=1, leave=True)
+        read_pbar = tqdm(len(filenames), desc="Reading Files", position=0, leave=False)
+        work_pbar = tqdm(len(filenames), desc="Processing Files", position=1, leave=True)
 
-    active_reads_Lock = threading.Lock()
-    active_reads = 0
-    pending_work_Lock = threading.Lock()
-    pending_work = 0
-    active_work_Lock = threading.Lock()
-    active_work = 0
-
-
-    def read_update(result):
-        global active_reads
-        global pending_work
-        global graphs_list
-        with active_reads_Lock:
-            active_reads -= 1
-        with pending_work_Lock:
-            pending_work += 1
-        graphs_list.append(result)
-        read_pbar.update()
+        active_reads_Lock = threading.Lock()
+        active_reads = 0
+        pending_work_Lock = threading.Lock()
+        pending_work = 0
+        active_work_Lock = threading.Lock()
+        active_work = 0
 
 
-    def work_update(result):
-        # store results in global lists
-        with results_lock:
-            global abs_js
-            global seq_js
-            global gen
-            #global M
-
-            abs_js.append(result[0])
-            seq_js.append(result[1])
-            gen += result[2]
-
-        # update work status variables
-        global active_work
-        with active_work_Lock:
-            active_work -= 1
-        work_pbar.update()
+        def read_update(result):
+            global active_reads
+            global pending_work
+            global graphs_list
+            with active_reads_Lock:
+                active_reads -= 1
+            with pending_work_Lock:
+                pending_work += 1
+            graphs_list.append(result)
+            read_pbar.update()
 
 
-    work_pool = mp.Pool(num)
+        def work_update(result):
+            # store results in global lists
+            with results_lock:
+                global abs_js
+                global seq_js
+                global gen
+                #global M
 
-    with mp.Pool(num) as read_pool:
-        while filenames or graphs_list:
-            if active_reads + pending_work + active_work <= num:
-                if filenames:
-                    filename = filenames.pop(0)  # take the first item
-                    active_reads += 1
-                    read_pool.apply_async(load_graph, [filename], callback=read_update)
-                    # graphs_list.append(read_update(load_graph(filename)))
-                for idx, graph in enumerate(graphs_list):
-                    active_work += 1
-                    work_pool.apply_async(parallel_thing, [graph], callback=work_update)
-                    graphs_list.pop(idx)
-                    pending_work -= 1
-            else:
-                for idx, graph in enumerate(graphs_list):
-                    active_work += 1
-                    work_pool.apply_async(parallel_thing, [graph], callback=work_update)
-                    graphs_list.pop(idx)
-                    pending_work -= 1
-                #ColorPrint.print_blue(f'Sleeping {active_reads}, {pending_work}, {active_work}')
-                time.sleep(10)
-    # wait until everything is off of the queue
-    while active_work > 0:
-        time.sleep(num)
+                abs_js.append(result[0])
+                seq_js.append(result[1])
+                gen += result[2]
 
-    work_pool.close()
+            # update work status variables
+            global active_work
+            with active_work_Lock:
+                active_work -= 1
+            work_pbar.update()
 
-    df = construct_table(abs_js, seq_js, gen, models[0])
-    df.to_csv(f'{output_path}/{dataset}_{models[0]}_js.csv', float_format='%.7f', sep='\t', index=False)
-    #df.to_csv(f'data-JS/tf_test_{dataset}_{models[0]}_js.csv', float_format='%.7f', sep='\t', index=False)
+
+        work_pool = mp.Pool(num)
+
+        with mp.Pool(num) as read_pool:
+            while filenames or graphs_list:
+                if active_reads + pending_work + active_work <= num:
+                    if filenames:
+                        filename = filenames.pop(0)  # take the first item
+                        active_reads += 1
+                        read_pool.apply_async(load_graph, [filename], callback=read_update)
+                        # graphs_list.append(read_update(load_graph(filename)))
+                    for idx, graph in enumerate(graphs_list):
+                        active_work += 1
+                        work_pool.apply_async(parallel_thing, [graph], callback=work_update)
+                        graphs_list.pop(idx)
+                        pending_work -= 1
+                else:
+                    for idx, graph in enumerate(graphs_list):
+                        active_work += 1
+                        work_pool.apply_async(parallel_thing, [graph], callback=work_update)
+                        graphs_list.pop(idx)
+                        pending_work -= 1
+                    #ColorPrint.print_blue(f'Sleeping {active_reads}, {pending_work}, {active_work}')
+                    time.sleep(10)
+        # wait until everything is off of the queue
+        while active_work > 0:
+            time.sleep(num)
+
+        work_pool.close()
+
+        df = construct_table(abs_js, seq_js, gen, models[0])
+        df.to_csv(f'{output_path}/{dataset}_{models[0]}_js.csv', float_format='%.7f', sep='\t', index=False)
