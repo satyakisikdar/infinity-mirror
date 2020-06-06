02619a8c870c413859fd3da98944b72d948cb662
Trenton W. Ford - Desktop <trentonford@gmail.com>
2020-06-06 16:26:47 -0400

    getting right with the commit

--- scripts/parallel/deltacon_parallel.py
+++ scripts/parallel/deltacon_parallel.py
@@ -18,7 +18,7 @@ def load_data(base_path, dataset, model, seq_flag, rob_flag):
     for subdir, dirs, files in os.walk(path):
         for filename in files:
             if 'csv' not in filename:
-                if 'seq' not in filename and 'rob' not in filename:
+                if 'seq' in filename and 'rob' not in filename:
                     print(f'loading {subdir} {filename} ... ', end='', flush=True)
                     pkl = load_pickle(os.path.join(subdir, filename))
                     trial = filename.split('_')[2].strip('.pkl.gz')
@@ -36,7 +36,7 @@ def mkdir_output(path):
 def compute_graph_stats(root):
     print('computing GraphStats... ', end='', flush=True)
     if type(root) is list:
-        graph_stats = [GraphStats(graph=g, run_id = 1) for g in root]
+        graph_stats = [GraphStats(graph=g, run_id=1) for g in root]
     else:
         graph_stats = [GraphStats(graph=node.graph, run_id=1) for node in [root] + list(root.descendants)]
     print('done')
@@ -108,30 +108,50 @@ def construct_full_table(abs_delta, seq_delta, model, trials):
 if __name__ == '__main__':
 
     base_path = '/data/infinity-mirror/'
-    dataset = 'tree'
-    models = ['BTER', 'BUGGE', 'CNRG', 'Chung-Lu', 'Erdos-Renyi', 'SBM', 'HRG', 'NetGAN']
+    dataset = 'clique-ring-500-4'
+    models = ['CNRG', 'Chung-Lu', 'Erdos-Renyi', 'SBM', 'HRG', 'NetGAN']
 
     output_path = os.path.join(base_path, 'stats/deltacon/')
     mkdir_output(output_path)
 
     cols = ['model', 'gen', 'trial_id', 'deltacon']
 
+    g1 = nx.ring_of_cliques(500, 4)
+    g2 = nx.ring_of_cliques(500, 4)
+
+    gpc1 = GraphPairCompare(GraphStats(g1, 1), GraphStats(g1, 1))
+    print('should def be 0', gpc1.deltacon0())
+
+    gpc2 = GraphPairCompare(GraphStats(g1, 1), GraphStats(g2, 1))
+    print('should probably be 0', gpc2.deltacon0())
+
+    exit()
 
     for model in models[: 1]:
         abs_delta = []
         trials = []
         rows = {col: [] for col in cols}
         for root, trial in load_data(base_path, dataset, model, True, False):
-            graph_stats = compute_graph_stats(root)
+            # graph_stats = compute_graph_stats(root)
+            if isinstance(root, list):
+                root_graph = root[0]
+                descendants = root[1: ]
+            else:
+                root_graph = root.graph
+                descendants = [tnode.graph for tnode in root.descendants]
 
-            for i, gs in enumerate(graph_stats[1: ], 1):
-                comparator = GraphPairCompare(graph_stats[0], gs)
-                delta = comparator.deltacon0()
+            root_graph = nx.convert_node_labels_to_integers(root_graph, first_label=0)
+            root_gstat = GraphStats(root_graph, 0)
+
+            for i, desc_g in enumerate(descendants, 1):
+                desc_g = nx.convert_node_labels_to_integers(desc_g, first_label=0)
+                comparator = GraphPairCompare(root_gstat, GraphStats(desc_g, 0))
+                delta = comparator.deltacon0(eps=0.001)
                 rows['model'].append(model)
                 rows['gen'].append(i)
-                rows['trial_id'].append(trial)
+                rows['trial_id'].append(int(trial))
                 rows['deltacon'].append(delta)
-
+            break
         df = pd.DataFrame(rows)
 
         df.to_csv(f'{output_path}/{dataset}_{model}_deltacon.csv', float_format='%.7f', sep='\t', index=False, na_rep='nan')
