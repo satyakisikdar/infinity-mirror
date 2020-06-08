386a192bb829cc6bdc66d6ee56d69261a337d1a2
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-06-08 19:40:51 -0400

    NetLSD repo 	new file:   NetLSD/LICENSE.txt 	new file:   NetLSD/MANIFEST.in 	new file:   NetLSD/README.rst 	new file:   NetLSD/docs/Makefile 	new file:   NetLSD/docs/authors.rst 	new file:   NetLSD/docs/conf.py 	new file:   NetLSD/docs/contributing.rst 	new file:   NetLSD/docs/index.rst 	new file:   NetLSD/docs/installation.rst 	new file:   NetLSD/docs/make.bat 	new file:   NetLSD/docs/netlsd.kernels.compare.rst 	new file:   NetLSD/docs/netlsd.kernels.heat.rst 	new file:   NetLSD/docs/netlsd.kernels.netlsd.rst 	new file:   NetLSD/docs/netlsd.kernels.wave.rst 	new file:   NetLSD/docs/readme.rst 	new file:   NetLSD/docs/reference.rst 	new file:   NetLSD/docs/usage.rst 	new file:   NetLSD/netlsd/__init__.py 	new file:   NetLSD/netlsd/kernels.py 	new file:   NetLSD/netlsd/util.py 	new file:   NetLSD/requirements.txt 	new file:   NetLSD/setup.cfg 	new file:   NetLSD/setup.py

---
+++ NetLSD/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='NetLSD',
+    version='1.0.2',
+    author='Anton Tsitsulin',
+    author_email='anton.tsitsulin@hpi.de',
+    description='NetLSD descriptors for graphs. Compare and analyze graph structure on multiple levels!',
+    packages=['netlsd',],
+    url='http://github.com/xgfs/netlsd',
+    download_url = 'https://github.com/xgfs/netlsd/archive/0.1.tar.gz',
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    long_description=open('README.rst').read(),
+)
