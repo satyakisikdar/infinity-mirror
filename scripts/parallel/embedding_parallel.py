863b540f765ab94eea1b3ba5366c4a9f83626275
Trenton W. Ford - Desktop <trentonford@gmail.com>
2020-06-15 11:58:14 -0400

    merge branch

--- scripts/parallel/embedding_parallel.py
+++ scripts/parallel/embedding_parallel.py
@@ -8,13 +8,13 @@ from tqdm import tqdm
 sys.path.append('./..')
 sys.path.append('./../../')
 import numpy as np
-import netlsd as net
+import NetLSD.netlsd as net
 import networkx as nx # requires 2.3.0
 import pandas as pd; pd.options.display.float_format = '{:,.5f}'.format
 import warnings; warnings.filterwarnings("ignore", category=UserWarning)
 from scipy.sparse.linalg.eigen.arpack import ArpackNoConvergence
 
-from src.utils import load_pickle, ColorPrint
+from src.utils import load_pickle, ColorPrint, verify_dir, verify_file
 import multiprocessing as mp
 
 
@@ -26,10 +26,11 @@ def compare_graphs(g1: nx.Graph, g2: nx.Graph, kernel: str='heat', dim: int=250,
     g_vec2 = get_graph_vec(g=g2, kernel=kernel, dim=dim, eigenvalues=eigenvalues)
     return net.compare(g_vec1, g_vec2)
 
-def get_row(root, cols, dataset, model, dim):
+#todo: what does this function do that the one in graph embedding doesn't?
+def get_row(root, cols, dataset, model, trial, dim):
     if type(root) is list:
         for idx, graph in enumerate(root):
-            row = {'name': dataset, 'level': idx, 'model': model}
+            row = {'name': dataset, 'level': idx, 'model': model, 'trial': trial}
             try:
                 for i, x in enumerate(get_graph_vec(graph, dim=dim)):
                     row[f'v{i}'] = x
@@ -56,27 +57,48 @@ def load_data(input_path, dataset, model):
         yield pkl, trial
 
 def parallel_computation(input_path, dataset, model):
+    output_path = os.path.join('/data/infinity-mirror/', 'output', 'embedding/' ,model)
+    verify_dir(output_path)
+
+    filename = f'{output_path}/embedding_{dataset}_{model}.csv'
+    if verify_file(filename):
+        df = pd.read_csv(filename, sep='\t')
+        return df, dataset, model
+
     rows = {col: [] for col in cols}
+
     for graph_list, trial in tqdm(load_data(input_path, dataset, model)):
+        temp_filename = f'{output_path}/embedding_{dataset}_{model}_temp_{trial}.csv'
         try:
-            for row in get_row(graph_list, cols, dataset, model, dim):
+            for row in get_row(graph_list, cols, dataset, model, trial, dim):
                 for key, val in row.items():
                     rows[key].append(val)
             # print('SUCCESS')
         except ArpackNoConvergence:
             print('FAILURE')
+        inc_df = pd.DataFrame(rows)
+        inc_df.to_csv(temp_filename, float_format='%.7f', sep='\t', index=False, na_rep='nan')
+
     df = pd.DataFrame(rows)    # save out incremental work in case something goes wrong
-    df.to_csv(f'{os.path.join(output_path,model)}/embedding_{dataset}_{model}.csv', float_format='%.7f', sep='\t', index=False, na_rep='nan')
+    df.to_csv(filename, float_format='%.7f', sep='\t', index=False, na_rep='nan')
     return df, dataset, model
 
 if __name__ == '__main__':
-    input_path = '/afs/crc.nd.edu/user/t/tford5/infinity-mirror/cleaned/'
-    output_path = os.path.join('/afs/crc.nd.edu/user/t/tford5/infinity-mirror/', 'output', 'embedding/')
-    datasets = ['eucore','tree', 'chess']
-    models = ['BTER', 'GCN_AE', 'Linear_AE']
+    #dac
+    # input_path = '/afs/crc.nd.edu/user/t/tford5/infinity-mirror/cleaned/'
+    #DSGs
+    input_path = '/data/infinity-mirror/cleaned/'
+
+    output_path = os.path.join('/data/infinity-mirror/', 'output', 'embedding/')
+    verify_dir(output_path)
+    datasets = ['eucore','tree', 'flights', 'clique-ring-500-4']
+    models = ['BTER', 'BUGGE', 'CNRG', 'Chung-Lu', 'Erdos-Renyi', 'HRG', 'SBM', 'NetGAN', 'GCN_AE', 'Linear_AE']
+    #test
+    # datasets = ['eucore']
+    # models = ['CNRG']
 
     dim = 250
-    cols = ['name', 'level', 'model'] + [f'v{i}' for i in range(dim)]
+    cols = ['name', 'level', 'model', 'trial'] + [f'v{i}' for i in range(dim)]
 
     parallel_args = []
     results = []
@@ -95,10 +117,11 @@ if __name__ == '__main__':
     # sequential implementation
     # for p_arg in parallel_args:
     #     results.append(parallel_computation(p_arg[0],p_arg[1],p_arg[2]))
-    #
+
     asyncResults = []
-    with mp.Pool(12) as outerPool:
-        ColorPrint.print_green(f"Starting Pool with {12} threads with {len(parallel_args)} tasks.")
+    n_threads = 4
+    with mp.Pool(n_threads) as outerPool:
+        ColorPrint.print_green(f"Starting Pool with {n_threads} threads with {len(parallel_args)} tasks.")
         for p_arg in parallel_args:
             r = outerPool.apply_async(parallel_computation, p_arg, callback=result_update)
             asyncResults.append(r)
