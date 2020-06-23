9d645c4c0c674a966e7ab27c7c055f605d5a90a8
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-06-23 02:47:55 -0400

    removed -f flag 	modified:   all_models.sh

--- all_models.sh
+++ all_models.sh
@@ -1,5 +1,5 @@
 models="Kronecker"  # space separated names of models, find the whole list in main.py
-graph='input/eucore.g'  # external edge list 
+graph='clique_ring 500 4'  # external edge list 
 # graph='clique_ring 500 4'  # for synthetic graphs - find list in Synthetic Graph class in src/graphio.py 
 for model in $models
 do
@@ -8,7 +8,7 @@ do
     # -s: selection strategy
     # -f: use an incomplete file as a starting point
     # -p: use pickles to prevent repeated computations
-    echo python3 main.py -i $graph -m $model -t 1 -n 20 -c 1 -s fast -f /data/infinity-mirror/buckets/bucket3/eucore/Kronecker/list_10_1.pkl.gz -p
-    python3 main.py -i $graph -m $model -t 1 -n 20 -c 1 -s fast -f /data/infinity-mirror/buckets/bucket3/eucore/Kronecker/list_10_1.pkl.gz -p
+    echo python3 main.py -i $graph -m $model -t 1 -n 20 -c 1 -s fast -p
+    python3 main.py -i $graph -m $model -t 1 -n 20 -c 1 -s fast -p
 done
 # output pickles are stored in infinity-mirror/output/pickles directory
