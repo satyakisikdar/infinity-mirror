b86cc2f8aef0588b683676196f295a1f5eac1980
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-06-26 13:55:02 -0400

    metrics -> stats 	modified:   src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -320,22 +320,22 @@ def walker():
 
 def walker_texas_ranger():
     base_path = os.join(get_imt_output_directory(), 'graph_stats')
-    datasets, models, metrics, trials, iterations, filenames = [], [], [], [], []
+    datasets, models, stats, trials, iterations, filenames = [], [], [], [], []
 
     for subdir, dirs, files in os.walk(base_path):
         for filename in files:
             subdir_list = subdir.split('/')
             dataset = subdir_list[-3]
             model = subdir_list[-2]
-            metric = subdir_list[-1]
+            stat = subdir_list[-1]
             trial = int(filename.split('_')[-2])
             iteration = int(filename.split('_')[-1].strip('.json.gz'))
 
             datasets.append(dataset)
             models.append(model)
-            metrics.append(metric)
+            stats.append(stat)
             trials.append(trial)
             iterations.append(iteration)
             filenames.append(filename)
 
-    return datasets, models, metrics, trials, iterations, filenames
+    return datasets, models, stats, trials, iterations, filenames
