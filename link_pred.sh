f8ca084c7a23d72e530ffd5398e30b8182e36832
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-07-15 14:07:23 -0400

    new simplified script for Justus 	new file:   link_pred.sh

---
+++ link_pred.sh
@@ -0,0 +1,16 @@
+models="GCN_AE"  # space separated names of models, find the whole list in main.py
+graph='input/karate.g'  # external edge list 
+# graph='clique_ring 500 4'  # for synthetic graphs - find list in Synthetic Graph class in src/graphio.py 
+for model in $models
+do
+    # -t: number of independent trials
+    # -c: number of cores
+    # -s: selection strategy
+    # -f: use an incomplete file as a starting point
+    # -p: use pickles to prevent repeated computations
+    # -z: return the features the model learns
+    echo python3 main.py -i $graph -m $model -t 1 -n 1 -c 1 -s fast -p -z
+    python3 main.py -i $graph -m $model -t 1 -n 1 -c 1 -s fast -p -z
+done
+# output graph pickles are stored in infinity-mirror/output/pickles directory
+# output feature pickles are stored in infinity-mirror/output/features directory
