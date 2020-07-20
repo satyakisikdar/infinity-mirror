4c4494afd9398bc98f36d7d2b50927937f25764c
cedré <daniel.gonzalez.cedre@gmail.com>
2020-07-20 15:26:38 -0400

    unset the take_lcc flag by default 	modified:   link_pred.sh

--- link_pred.sh
+++ link_pred.sh
@@ -9,8 +9,8 @@ do
     # -p: use pickles to prevent repeated computations
     # -z: return the features the model learns
     # -l: whether or not to take only the largest connected component
-    echo python3 main.py -i $graph -m $model -t 1 -n 1 -c 1 -s fast -p -z -l
-    python3 main.py -i $graph -m $model -t 1 -n 1 -c 1 -s fast -p -z -l
+    echo python3 main.py -i $graph -m $model -t 1 -n 1 -c 1 -s fast -p -z
+    python3 main.py -i $graph -m $model -t 1 -n 1 -c 1 -s fast -p -z
 done
 # output graph pickles are stored in infinity-mirror/output/pickles directory
 # output feature pickles are stored in infinity-mirror/output/features directory
