18e49984be6625bb17aef5ea0d46abd4cef11d15
satyakisikdar <ssikdar@nd.edu>
2021-06-10 15:44:42 -0400

    try catch for graph distance computation, updated implemented methods to include portrait and lambda distance

--- src/parallel_graph_distance_driver.py
+++ src/parallel_graph_distance_driver.py
@@ -27,13 +27,16 @@ def distance_computation(dataset, model, trial, stats):
 
         for iteration in range(total_iterations+1):
             GD.set_iteration(iteration=iteration)
-            GD.compute_distances([stat])
+            try:
+                GD.compute_distances([stat])
+            except Exception as e:
+                print(f'Error in distance computation {e}')
+                continue
             results = GD.stats[stat]
 
             row = {}
             row.update({'dataset': dataset, 'model': model, 'trial': trial, 'iteration': iteration, stat: results})
             rows.append(row)
-
 
         results_df = pd.DataFrame(rows)
         # results_df.to_csv(path_or_buf=f'{output_dir}/{stat}_{trial}.csv', index=False)
@@ -42,18 +45,23 @@ def distance_computation(dataset, model, trial, stats):
 
 # TODO: fix support for HRG (no objects to contatenate)
 if __name__ == '__main__':
-    implemented_metrics = {'pagerank_js': 'pagerank', 'degree_js': 'degree_dist',
-                           # 'pgd_distance': 'pgd_graphlet_counts', 'netlsd_distance': 'netlsd', 'portrait_divergence': 'portrait',
+    implemented_metrics = {'pagerank_js': 'pagerank', 'degree_js': 'degree_dist', 'pgd_distance': 'pgd_graphlet_counts',
+                            'netlsd_distance': 'netlsd', 'portrait_divergence': 'b_matrix',
                            'lambda_distance': 'laplacian_eigenvalues'}
 
     # datasets = ['clique-ring-500-4', 'eucore', 'flights', 'tree']
 
-    models = ['Chung-Lu', 'CNRG', 'SBM', 'Erdos-Renyi', 'BUGGE', 'HRG']
-    # models = ['BTER', 'BUGGE', 'Chung-Lu', 'CNRG', 'Erdos-Renyi', 'Kronecker', 'SBM', 'GCN_AE', 'Linear_AE']
+    # models = ['Chung-Lu', 'CNRG', 'SBM', 'Erdos-Renyi', 'BUGGE', 'HRG'][: 1]
+    models = ['BTER', 'BUGGE', 'Chung-Lu', 'CNRG', 'Erdos-Renyi', 'Kronecker', 'HRG', 'SBM', 'GCN_AE', 'Linear_AE',
+              'NetGAN', 'GraphRNN']
     #stats = ['pagerank_js', 'degree_js', 'pgd_distance', 'netlsd_distance', 'lambda_distance', 'portrait_divergence']
-    stats = ['degree_js', 'pagerank_js', 'lambda_distance']
+    stats = ['degree_js', 'pagerank_js', 'lambda_distance', 'portrait_divergence']
     # datasets, models, trials, filenames = walker()
-    datasets = ['cond-mat', 'enron']
+    datasets = ['eucore', 'chess', 'flights', 'cond-mat', 'enron']#[-2: -1]
+
+    datasets = ['cond-mat']
+    # stats = ['pagerank_js']
+    # models = ['CNRG']
 
     for dataset in datasets:
         for model in models:
@@ -61,17 +69,16 @@ if __name__ == '__main__':
                 ColorPrint.print_green(f'computing {stat} distances for {dataset} {model}')
                 trials = walker_texas_ranger(dataset, model, stat=implemented_metrics[stat], unique=True)
                 args = [[dataset, model, trial, stat] for trial in trials]
-                print(args[: 5])
                 # exit(-1)
                 try:
-                    results = parallel_async(distance_computation, args, num_workers=10)
+                    results = parallel_async(distance_computation, args, num_workers=8)
                     df = pd.concat(results)
                 except Exception as e:
-                    ColorPrint.print_red(f'Error, for {dataset!r} {model!r} {stat!r}')
+                    ColorPrint.print_red(f'Error, for {dataset!r} {model!r} {stat!r} {e}')
                     continue
 
                 # output_dir = f'/data/infinity-mirror/output/distances/{dataset}/{model}/{stat}/'
-                output_dir = Path(get_imt_output_directory()) / 'distances' / dataset
+                output_dir = Path(get_imt_output_directory()) / 'new-distances' / stat
                 ensure_dir(output_dir, recursive=True)
                 df.to_csv(output_dir / f'{dataset}_{model}_{stat}.csv')
                 # for arg in args:
